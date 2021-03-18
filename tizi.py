ada8defcd673665ea242b6e2c13a22b6f2669d06
nanosoc <70638644+nanosoc@users.noreply.github.com>
2021-03-19 02:08:10 +0800

    Update tizi.py

--- tizi.py
+++ tizi.py
@@ -1,15 +1,22 @@
-import requests, json, random, re, time,selenium,os
+import requests, json, random, re, time,selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-TG_BOT_TOKEN = ''           # telegram bot token 自行申请
-TG_USER_ID = ''             # telegram 用户ID
+'''
+chrome_opt = webdriver.ChromeOptions()
+chrome_opt.add_argument('--headless')  # 无界面化.
+chrome_opt.add_argument('--disable-gpu')  # 配合上面的无界面化.
+chrome_opt.add_argument('--window-size=1366,768')  # 设置窗口大小, 窗口大小会有影响.
+chrome_opt.add_argument("--no-sandbox")
+driver = webdriver.Chrome(options=chrome_opt)
+'''
+
 def get_email():
     a = random.randint(11, 99)
     b = random.randint(0, 20)
-    email = 'lengtmp+{}no{}t@gmail.com'.format(a, b)
+    email = 'varytmp+{}no{}d@gmail.com'.format(a, b)
     return email
 def send(email):
     data = {
@@ -36,7 +43,6 @@ def get_num(email):
         #print(s_url)
         Driver.get(s_url)
         time.sleep(2)
-        telegram_bot("梯子", '邀请成功！')
         Driver.quit()
     except Exception as e:
         print(e)
@@ -56,7 +62,6 @@ def register(email):
         Driver.get('https://www.zionladdero.com/register')
         #driver.switch_to.frame('//*[@id="register"]/div/div')
         WebDriverWait(Driver,10).until(EC.element_to_be_clickable((By.ID, 'register')))
-        time.sleep(5)
         Driver.find_element_by_xpath('//*[@id="id_username"]').send_keys(email)
         Driver.find_element_by_xpath('//*[@id="id_password1"]').send_keys('refddr265rt!')
         Driver.find_element_by_xpath('//*[@id="id_password2"]').send_keys('refddr265rt!')
@@ -98,23 +103,6 @@ def register2(email,invite):
         time.sleep(5)
     except Exception as e:
         print(e)
-        telegram_bot("梯子", '邀请失败！')
-def telegram_bot(title, content):
-    print("\n")
-    tg_bot_token = TG_BOT_TOKEN
-    tg_user_id = TG_USER_ID
-    if "TG_BOT_TOKEN" in os.environ and "TG_USER_ID" in os.environ:
-        tg_bot_token = os.environ["TG_BOT_TOKEN"]
-        tg_user_id = os.environ["TG_USER_ID"]
-    if not tg_bot_token or not tg_user_id:
-        print("Telegram推送的tg_bot_token或者tg_user_id未设置!!\n取消推送")
-        return
-    print("Telegram 推送开始")
-    send_data = {"chat_id": tg_user_id, "text": title +
-                 '\n\n'+content, "disable_web_page_preview": "true"}
-    response = requests.post(
-        url='https://api.telegram.org/bot%s/sendMessage' % (tg_bot_token), data=send_data)
-    print(response.text)
 def main():
     email=get_email()
     ssr=register(email)
